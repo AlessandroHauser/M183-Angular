2a8132dd4b6bf113e16ef9549eef1c36d8559a2b
ShadowMega1337 <alessandro.hauser@bbzbl-it.ch>
2024-06-18 09:08:05 +0200

    fix security vulnerabilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 import {MatButton} from "@angular/material/button";
 import {TodoListComponent} from "./list/todo-list/todo-list.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-    imports: [RouterOutlet, MatButton, TodoListComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [MatButton, TodoListComponent],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'M183';
+    title = 'M183';
 
     login() {
         console.log("logging in")
