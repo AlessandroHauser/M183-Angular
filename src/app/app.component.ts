b6900a8cd9cd7933907c0fe8b2726adcfe3b80e9
fynnhaenybbzbl <108476679+fynnhaenybbzbl@users.noreply.github.com>
2024-06-18 10:28:05 +0200

    Changes by user add sec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,20 @@
 import {Component} from '@angular/core';
-import {MatButton} from "@angular/material/button";
-import {TodoListComponent} from "./list/todo-list/todo-list.component";
+import {LoginComponent} from "./pages/login/login.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
     selector: 'app-root',
-    standalone: true,
-    imports: [MatButton, TodoListComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent {
     title = 'M183';
 
+    constructor(private dialog: MatDialog) {
+    }
+
     login() {
+        this.dialog.open(LoginComponent);
         console.log("logging in")
     }
 }
--- src/app/list/todo-list/todo-list.component.ts
+++ src/app/list/todo-list/todo-list.component.ts
@@ -7,22 +7,6 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
     selector: 'app-todo-list',
-    standalone: true,
-    imports: [
-        MatTable,
-        NgForOf,
-        MatButton,
-        MatColumnDef,
-        MatHeaderCell,
-        MatCell,
-        MatHeaderRow,
-        MatRow,
-        MatHeaderCellDef,
-        MatCellDef,
-        MatHeaderRowDef,
-        MatRowDef,
-        DatePipe
-    ],
     templateUrl: './todo-list.component.html',
     styleUrl: './todo-list.component.css'
 })
@@ -39,28 +23,6 @@ export class TodoListComponent implements OnInit {
         this.todos$.subscribe(todos => {
             this.todos = todos;
         })
-        this.todos = [
-            {id: 1, task: 'Buy groceries', status: Status.IN_PROGRESS, date: new Date()},
-            {id: 2, task: 'Clean the house', status: Status.DONE, date: new Date()},
-            {id: 3, task: 'Finish project report', status: Status.IN_PROGRESS, date: new Date()},
-            {id: 4, task: 'Schedule meeting with team', status: Status.DONE, date: new Date()},
-            {id: 5, task: 'Prepare presentation slides', status: Status.IN_PROGRESS, date: new Date()},
-            {id: 6, task: 'Call the plumber', status: Status.DONE, date: new Date()},
-            {id: 7, task: 'Respond to emails', status: Status.IN_PROGRESS, date: new Date()},
-            {id: 8, task: 'Complete the budget analysis', status: Status.DONE, date: new Date()},
-            {id: 9, task: 'Write a blog post', status: Status.IN_PROGRESS, date: new Date()},
-            {id: 10, task: 'Plan the weekend trip', status: Status.DONE, date: new Date()},
-            {id: 11, task: 'Attend the workshop', status: Status.IN_PROGRESS, date: new Date()},
-            {id: 12, task: 'Read the new book', status: Status.DONE, date: new Date()},
-            {id: 13, task: 'Update the website', status: Status.IN_PROGRESS, date: new Date()},
-            {id: 14, task: 'Organize the desk', status: Status.DONE, date: new Date()},
-            {id: 15, task: 'Send the birthday card', status: Status.IN_PROGRESS, date: new Date()},
-            {id: 16, task: 'Fix the bug in the code', status: Status.DONE, date: new Date()},
-            {id: 17, task: 'Practice yoga', status: Status.IN_PROGRESS, date: new Date()},
-            {id: 18, task: 'Prepare for the interview', status: Status.DONE, date: new Date()},
-            {id: 19, task: 'Renew the subscription', status: Status.IN_PROGRESS, date: new Date()},
-            {id: 20, task: 'Backup the files', status: Status.DONE, date: new Date()}
-        ];
     }
 
     createTodo(task: string) {
@@ -79,7 +41,6 @@ export class TodoListComponent implements OnInit {
         textAreaElement.disabled = true;
         todo.task = textAreaElement.value;
         this.sendUpdate(todo)
-        console.log(todo)
     }
 
     updateStatus(todo: Todo) {
